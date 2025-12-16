af9378a87f2cf5376c9006b73d81f493f5df5fdc
maria cindy <202303008@student.stikomyos.ac.id>
2025-12-16 09:58:42 +0700

    edit file index

---
+++ cindy.php
@@ -0,0 +1,12 @@
+<?php
+$nama = "Cindy";
+$nim = "230123456";
+$prodi = "Teknik Informatika";
+$warna = "Biru";
+
+echo "<h2>Data Mahasiswa</h2>";
+echo "Nama : $nama <br>";
+echo "NIM : $nim <br>";
+echo "Program Studi : $prodi <br>";
+echo "Warna Kesukaan : $warna <br>";
+?>
